16bf45aa220398dcb6042b9987d86f0008ce7821
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2019-12-01 17:42:46 +0100

    Ultimo Push

--- MyApplicationAAA1/app/src/main/java/com/example/myapplicationaaa1/activity/MainActivity.kt
+++ MyApplicationAAA1/app/src/main/java/com/example/myapplicationaaa1/activity/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.myapplicationaaa1.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplicationaaa1.R
 import com.example.myapplicationaaa1.fragment.Login_Fragment
 import com.example.myapplicationaaa1.fragment.News_Fragment
@@ -15,39 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseAnalytics.getInstance(this).logEvent("AppOpen",null)
+        FirebaseAnalytics.getInstance(this).logEvent("AppOpen", null)
 
         //Para inicar el gragmentlayout con algo al inicio
-        val profileFragment= Login_Fragment()
-        val fragmentTransaction =supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(fragmentContainer.id,profileFragment)
+        val profileFragment = Login_Fragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(fragmentContainer.id, profileFragment)
         fragmentTransaction.commit()
 
 
-        bottomNavView.setOnNavigationItemSelectedListener {
-            menuItem->
-//            val parametres =Bundle()
+        bottomNavView.setOnNavigationItemSelectedListener { menuItem ->
+            //            val parametres =Bundle()
 //                parametres.putString("tab","chat")
 
-            when(menuItem.itemId){
-                R.id.chat ->{
-                    FirebaseAnalytics.getInstance(this).logEvent("CHAT_TabClicked",null)
+            when (menuItem.itemId) {
+                R.id.chat -> {
+                    FirebaseAnalytics.getInstance(this).logEvent("CHAT_TabClicked", null)
 
                 }
-                R.id.home ->{
-                    FirebaseAnalytics.getInstance(this).logEvent("HOME_TabClicked",null)
+                R.id.home -> {
+                    FirebaseAnalytics.getInstance(this).logEvent("HOME_TabClicked", null)
 
-                    val newsFragment= News_Fragment()
-                    val fragmentTransactionNews =supportFragmentManager.beginTransaction()
-                    fragmentTransactionNews.replace(fragmentContainer.id,newsFragment)
+                    val newsFragment = News_Fragment()
+                    val fragmentTransactionNews = supportFragmentManager.beginTransaction()
+                    fragmentTransactionNews.replace(fragmentContainer.id, newsFragment)
                     fragmentTransactionNews.commit()
                 }
-                R.id.profile ->{
-                    FirebaseAnalytics.getInstance(this).logEvent("PROFILE_TabClicked",null)
+                R.id.profile -> {
+                    FirebaseAnalytics.getInstance(this).logEvent("PROFILE_TabClicked", null)
 
-                    val profileFragment= Login_Fragment()
-                    val fragmentTransaction =supportFragmentManager.beginTransaction()
-                    fragmentTransaction.replace(fragmentContainer.id,profileFragment)
+                    val profileFragment = Login_Fragment()
+                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+                    fragmentTransaction.replace(fragmentContainer.id, profileFragment)
                     fragmentTransaction.commit()
 
                 }
@@ -55,8 +53,6 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
-
 
 
     }
--- MyApplicationAAA1/app/src/main/java/com/example/myapplicationaaa1/activity/RegisterActivity.kt
+++ MyApplicationAAA1/app/src/main/java/com/example/myapplicationaaa1/activity/RegisterActivity.kt
@@ -1,36 +1,28 @@
 package com.example.myapplicationaaa1.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplicationaaa1.R
 import com.example.myapplicationaaa1.model.UserModel
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        progressBar3.visibility= View.GONE
+        progressBar3.visibility = View.GONE
 
-        registerButton.setOnClickListener{
+        registerButton.setOnClickListener {
             createNewUser()
-            FirebaseAnalytics.getInstance(this).logEvent("Register_Try",null)
+            FirebaseAnalytics.getInstance(this).logEvent("Register_Try", null)
 
         }
 
@@ -38,32 +30,27 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-    private fun createNewUser(){
+    private fun createNewUser() {
 
-        val username=usernameEditText.text.toString()
-        val email=emailEditText.text.toString()
-        val password=passwordEditText.text.toString()
+        val username = usernameEditText.text.toString()
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
 
-        if(username.isEmpty()) {
-            Toast.makeText(this,"Please Enter UserName", Toast.LENGTH_LONG).show()
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Please Enter UserName", Toast.LENGTH_LONG).show()
             return
-        }
-        else if(email.isEmpty()){
-            Toast.makeText(this,"Please Enter Email", Toast.LENGTH_LONG).show()
+        } else if (email.isEmpty()) {
+            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_LONG).show()
             return
-        }
-
-        else if(password.isEmpty()){
-            Toast.makeText(this,"Please Enter Password", Toast.LENGTH_LONG).show()
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_LONG).show()
             return
-        }
-
-        else{
+        } else {
             //Progress bar
-            progressBar3.visibility= View.VISIBLE
+            progressBar3.visibility = View.VISIBLE
 
             //Register with
-            val auth=FirebaseAuth.getInstance()
+            val auth = FirebaseAuth.getInstance()
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
@@ -71,42 +58,77 @@ class RegisterActivity : AppCompatActivity() {
                         val user = auth.currentUser
 
                         //Aqui???Register in FiraStore
-                        val dataBase= FirebaseFirestore.getInstance();
-                        val newUser=UserModel(userID = "",
+                        val dataBase = FirebaseFirestore.getInstance();
+                        val newUser = UserModel(
+                            userID = "",
                             //stroe this into a constant
-                            userName = username,url = "https://firebasestorage.googleapis.com/v0/b/alejandroluquegamecompanion.appspot.com/o/public%2Fimages%2Favatars%2Fic_ImageProfileDefault-web.png?alt=media&token=82231fc0-1e0d-4ffe-b4ac-f4677ee8e64b",
-                            password= password,
+                            userName = username,
+                            url = "https://firebasestorage.googleapis.com/v0/b/alejandroluquegamecompanion.appspot.com/o/public%2Fimages%2Favatars%2Fic_ImageProfileDefault-web.png?alt=media&token=82231fc0-1e0d-4ffe-b4ac-f4677ee8e64b",
+                            password = password,
                             email = email
                         )
-                        if (user !== null){
+                        if (user !== null) {
                             dataBase
                                 .collection("Users")
                                 .document(user.uid)
                                 .set(newUser)
-                                .addOnSuccessListener {documentReference ->
-                                    Toast.makeText(this,"Register Complete", Toast.LENGTH_LONG).show()
-                                    progressBar3.visibility= View.GONE
-                                    FirebaseAnalytics.getInstance(this).logEvent("Register_Complete",null)
+                                .addOnSuccessListener { documentReference ->
+                                    Toast.makeText(this, "Register Complete", Toast.LENGTH_LONG)
+                                        .show()
+                                    progressBar3.visibility = View.GONE
+                                    FirebaseAnalytics.getInstance(this)
+                                        .logEvent("Register_Complete", null)
 
-
+                                    applicationContext.getSharedPreferences(
+                                        "userProfile",
+                                        Context.MODE_PRIVATE
+                                    ).edit()
+                                        .putString("username", username).apply()
+                                    applicationContext.getSharedPreferences(
+                                        "userProfile",
+                                        Context.MODE_PRIVATE
+                                    ).edit()
+                                        .putString("password", password).apply()
+                                    applicationContext.getSharedPreferences(
+                                        "userProfile",
+                                        Context.MODE_PRIVATE
+                                    ).edit()
+                                        .putString("email", email).apply()
+                                    applicationContext.getSharedPreferences(
+                                        "userProfile",
+                                        Context.MODE_PRIVATE
+                                    ).edit()
+                                        .putString(
+                                            "image_url",
+                                            "https://firebasestorage.googleapis.com/v0/b/alejandroluquegamecompanion.appspot.com/o/public%2Fimages%2Favatars%2Fic_ImageProfileDefault-web.png?alt=media&token=82231fc0-1e0d-4ffe-b4ac-f4677ee8e64b"
+                                        ).apply()
+                                    progressBar3.visibility = View.GONE
 
                                 }.addOnFailureListener { e ->
-                                    Toast.makeText(this,"Error, try again later", Toast.LENGTH_LONG).show()
-                                    progressBar3.visibility= View.GONE
+                                    Toast.makeText(
+                                        this,
+                                        "Error, try again later",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    progressBar3.visibility = View.GONE
 
                                 }
                         }
 
                     } else {
                         // If sign in fails, display a message to the user.
-                        Toast.makeText(this,"Authentication failed.", Toast.LENGTH_LONG).show()
-                        progressBar3.visibility= View.GONE
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_LONG).show()
+                        progressBar3.visibility = View.GONE
 
                     }
-                }.addOnFailureListener(){
-                    Toast.makeText(this,"Error, try to increase the username or password", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener() {
+                    Toast.makeText(
+                        this,
+                        "Error, try to increase the username or password",
+                        Toast.LENGTH_LONG
+                    ).show()
 
-                    progressBar3.visibility= View.GONE
+                    progressBar3.visibility = View.GONE
 
                 }
 
@@ -115,8 +137,6 @@ class RegisterActivity : AppCompatActivity() {
 
 
     }
-
-
 
 
 }
